a7e66808b499ac92d399e520b38682f0aa48da06
Samyog <samyogk2081@gmail.com>
2026-09-22 23:24:12 +0545

    c

--- ws.cpp
+++ ws.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 int main() {
-    for(int i=1; i<=3; i++){
-        for(int j=1; j<=20; j+=2){
-            std::cout << i << " " << j << std::endl;
-        }
-        std::cout << std::endl;
-    }
+
     return 0;
 }
