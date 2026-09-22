39c39a077b5f96db2a85b4da32970a4048ebca17
Samyog <samyogk2081@gmail.com>
2026-09-22 23:23:36 +0545

    p1commit

---
+++ nestedloop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main() {
+    int rows;
+    int columns;
+    char symbol;
+
+    std::cout << "Enter the number of rows: ";
+    std::cin >> rows;
+
+    std::cout<<"Enter the number of columns: ";
+    std::cin>>columns;
+
+    std::cout<<"Enter the symbol to use: ";
+    std::cin>>symbol;
+
+    for(int i=1; i<=rows; i++){
+        for(int j=1; j<=columns; j++){
+            std::cout<<symbol;
+        }
+        std::cout<<std::endl;
+    }
+    return 0;
+}
