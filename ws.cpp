71469e03eea717aa05d6f08af655c5c865fd44a2
Samyog <samyogk2081@gmail.com>
2026-09-23 22:52:48 +0545

    .

--- ws.cpp
+++ ws.cpp
@@ -12,6 +12,16 @@ int num3 = (rand() % 6) + 1;
  std::cout<<"Dice roll: "<< num2 << std::endl;
  std::cout<<"Dice roll: "<< num3 << std::endl;
 
+if(num1 == num2 && num2 == num3) {
+    std::cout<<"JACKPOT"<< std::endl;
+}
+else if(num1 == num2 || num1 == num3 || num2 == num3) {
+    std::cout<<"You rolled 2 matching dice!"<< std::endl;
+}
+else {
+    std::cout<<"No matches,better luck next time :( "<< std::endl;
+}
+
 
     return 0;
 }
