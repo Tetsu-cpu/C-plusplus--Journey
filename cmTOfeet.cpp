0d1ff2d6bc0b324e1ca0a17dead0448995039d4d
Samyog <samyogk2081@gmail.com>
2026-09-26 22:33:20 +0545

    .

--- cmTOfeet.cpp
+++ cmTOfeet.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <cmath>
 
@@ -28,35 +27,3 @@ if(unit == 'F'){
    else{
 std::cout<<"Only Feet and Centimeters"<<'\n'; }
 return 0;
-=======
-#include <iostream>
-#include <cmath>
-
-int main(){
-   
-double height;
-char unit;
-
-std::cout<<"********HEIGHT CONVERSION********"<<std::endl;
-std::cout<<"F=FEET"<<std::endl;
-std::cout<<"C=CENTIMETERS"<<std::endl;
-std::cout<<"What unit would you like to convert to: ";
-std::cin>>unit;
-
-if(unit == 'F'){
-    std::cout<<"Enter in Centimeters: ";
-    std::cin>>height;
-    height= height/30.48;
-    std::cout<<"Height is: "<<height<<" Feet\n";
-   }
-   else if(unit =='C'){
-    std::cout<<"Enter in Feet: ";
-    std::cin>>height;
-    height= height*30.48;
-    std::cout<<"Height is: "<<height<<" Centimeters\n";
-   }
-   else{
-std::cout<<"Only Feet and Centimeters"<<'\n'; }
-return 0;
->>>>>>> 41291e37d004ee7623977c7b886fabd6b49942de
-}
